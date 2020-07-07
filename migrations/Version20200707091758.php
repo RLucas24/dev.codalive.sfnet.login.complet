b2392666743162606db1b8367536b2b7b3823a11
robert <fenosonrobert@gmail.com>
2020-07-07 15:27:30 +0300

    login user fount

--- migrations/Version20200707091758.php
+++ migrations/Version20200707091758.php
@@ -10,17 +10,18 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200707082946 extends AbstractMigration
+final class Version20200707091758 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'create user table';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
     }
 
     public function down(Schema $schema) : void
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,15 +2,28 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 Use sYmfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\PasswordUpgradeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginFormAuthenticator implements AuthenticatorInterface
 {
+
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository= $userRepository;
+    }
     /**
      * Does the authenticator support the given Request?
      *
@@ -39,7 +52,19 @@ class LoginFormAuthenticator implements AuthenticatorInterface
      */
     public function authenticate(Request $request): PassportInterface
     {
-        dd('authenticate');
+        // find a user based on an "email" form field
+        $user = $this->userRepository->findOneByEmail($request->request->get('email'));
+        if (!$user) {
+            throw new CustomUserMessageAuthenticationException('Invalid credential!!');
+        }
+
+        return new Passport($user, new PasswordCredentials($request->get('password')), [
+            // and CSRF protection using a "csrf_token" field
+            new CsrfTokenBadge('loginform', $request->get('csrf_token'))
+
+            // and add support for upgrading the password hash
+            //new PasswordUpgradeBadge($request->get('password'), $this->userRepository)
+        ]);
     }
 
     /**
@@ -69,7 +94,7 @@ class LoginFormAuthenticator implements AuthenticatorInterface
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-
+        dd('succes!!');
     }
 
     /**
@@ -83,7 +108,7 @@ class LoginFormAuthenticator implements AuthenticatorInterface
      */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-
+        dd('failure!!');
     }
     
 }
